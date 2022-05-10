e830bde7d3d8cc4ddbd4aba47f9482cb28e8f105
davidcoats <david@getsafetycone.com>
2022-05-10 10:53:58 -0700

    Initial commit of source-stub.

---
+++ source/R5T.B0007.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0007.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Spacing operations as ISpacingOperator base extensions..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.B0007.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0007.X0001
+{
+    /// <summary>
+    /// Spacing operations as ISpacingOperator base extensions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
